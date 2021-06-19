1fc4076b524679d37bd359d92637f0e3f9d646a8
soumyakumari74 <ksoumyaofficial@gmail.com>
2021-06-19 15:27:20 +0530

    Encryption

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
+const encrypt=require("mongoose-encryption");
+
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema={
+const userSchema=new mongoose.Schema({
   email:String,
   password:String
-};
+});
+
+const secret="Thisisourlittlesecret.";
+userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
 
 const User=new mongoose.model("User",userSchema);
 
